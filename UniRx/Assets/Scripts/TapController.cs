4ab256f8675fbb0af9014ae099f28a3fed11015a
藤田 幸弥 <s4ch1mos@gmail.com>
2020-05-06 22:09:58 +0900

    sinngleタップとダブルタップの判定

---
+++ UniRx/Assets/Scripts/TapController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapController : MonoBehaviour
+{
+    [SerializeField] private Button tapButton;
+    [SerializeField] private Text counter;
+    [SerializeField] private Button singleTapButton;
+    [SerializeField] private Text singleCounter;
+    [SerializeField] private Text judge;
+    
+    private int _count = 0;
+    private int _singleCount = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        singleTapButton.OnSafeClickAsObservable()
+            .Subscribe(_ => CountSingle())
+            .AddTo(this);
+
+        tapButton.OnClickAsObservable()
+            .Subscribe(_ => Count())
+            .AddTo(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Count()
+    {
+        _count++;
+        counter.text = _count.ToString();
+        judge.text = "tap";
+    }
+    
+    private void CountSingle()
+    {
+        _singleCount++;
+        singleCounter.text = _singleCount.ToString();
+        judge.text = "single tap";
+    }
+}
---
+++ UniRx/Assets/Scripts/UniRx/UniRxExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class UniRxExtensions
+{
+    #region method
+
+    /// <summary>
+    /// 連打防止用クリック監視
+    /// </summary>
+    public static IObservable<Unit> OnSafeClickAsObservable(this Button button)
+    {
+        return button.onClick.AsObservable().ThrottleFirst(System.TimeSpan.FromMilliseconds(250));
+    }
+    
+    #endregion
+}
